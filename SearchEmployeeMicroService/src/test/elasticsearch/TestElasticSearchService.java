c2709863ffd07fc4c3ec8ec236be8ae926aa511c
shardulbho <shardulbho@cybage.com>
2017-06-13 12:32:34 +0530

    Implemented JUnit

--- SearchEmployeeMicroService/src/test/elasticsearch/TestElasticSearchService.java
+++ SearchEmployeeMicroService/src/test/elasticsearch/TestElasticSearchService.java
@@ -1,17 +1,58 @@
 package test.elasticsearch;
 
-import com.searchemployeeservice.util.ElasticSearchUtil;
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.stereotype.Component;
+
+import com.searchemployeeservice.bean.Employee;
+import com.searchemployeeservice.service.ElasticSearchService;
+
+@Component
 public class TestElasticSearchService {
 
-	public static void main(String[] args) {
-		
+	ElasticSearchService elasticSearchService = new ElasticSearchService();
+	
+	@Test
+	public void searchEmployeeTest(){
 		try {
-			String response ="";// ElasticSearchUtil.searchEmployee("10");
+			List<Employee> empList =  elasticSearchService.searchEmployee("010");
+			List<Employee> empListExpected = new ArrayList<Employee>();
+			Employee e = new Employee();
+			e.setEmpFirstName("Monica");
+			e.setEmailID("monica.gellar@friends.com");
+			e.setEmpId("010");
+			e.setEmpLastName("Gellar");
+			e.setPhoneNo(Long.parseLong("9890123456"));
 			
-			System.out.println("Response is "+response);
+			
+			empListExpected.add(e);
+			assertEquals(empListExpected.get(0).getEmpId(),empList.get(0).getEmpId());
+			assertEquals(empListExpected.get(0).getEmpFirstName(),empList.get(0).getEmpFirstName());
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void saveEmployeeDetails(){
+		Employee expected = new Employee();
+		expected.setEmpFirstName("Shardul");
+		expected.setEmailID("Shardul.B@friends.com");
+		expected.setEmpId("999");
+		expected.setEmpLastName("B");
+		expected.setPhoneNo(Long.parseLong("0000000001"));
+		
+		try {
+			elasticSearchService.saveEmployeeDetails(expected);
+			List<Employee> empList =  elasticSearchService.searchEmployee("999");
+			assertEquals(expected.getEmpId(),empList.get(0).getEmpId());
+			assertEquals(expected.getEmpFirstName(),empList.get(0).getEmpFirstName());
+		} catch (Exception e1) {
+			e1.printStackTrace();
 		}
 	}
 }
